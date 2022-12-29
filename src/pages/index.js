6f120c673abc550eecd65e313d146fbd5a59459b
WAQARRKHANN <47204538+WAQARRKHANN@users.noreply.github.com>
2022-12-29 22:57:30 +0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import DarkTheme from '../layouts/Dark'
+import Demos from "./demos";
+import Homepage1 from './home/home1-dark';
+
+export default function Home() {
+  return (
+    <DarkTheme>
+      <Homepage1 />
+      {/* <Demos /> */}
+    </DarkTheme>
+  )
+}
